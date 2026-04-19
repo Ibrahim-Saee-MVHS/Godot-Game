97123db6e91b1faaf318833fa4b5c6be5104d5b7
Finn10Plays <s0024@mvhs.edu.mv>
2026-04-19 17:31:50 +0500

    Dash VFX looks more cooler

--- scripts/dash.gd
+++ scripts/dash.gd
@@ -9,6 +9,9 @@ func _ready():
 func _process(_delta):
 	if Abilities.abilityTimer <= 0:
 		$VFX.emitting = false
+		if top_level == false:
+			top_level = true
+			global_position = get_parent().global_position
 
 func _on_cpu_particles_2d_finished():
 	queue_free()
