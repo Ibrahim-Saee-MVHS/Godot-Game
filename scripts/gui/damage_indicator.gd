a7f5603cd0928e92f989087f778503e0d02580d7
Ibrahim-Saee-MVHS <s0024@mvhs.edu.mv>
2026-04-21 10:03:11 +0500

    Added Outline to Damage Indicator

--- scripts/gui/damage_indicator.gd
+++ scripts/gui/damage_indicator.gd
@@ -4,10 +4,11 @@ var DAMAGE: float
 var velocity: float = 20
 
 func _process(delta: float) -> void:
-	if !is_equal_approx(DAMAGE, round(DAMAGE)):
-		text = str(snapped(DAMAGE, 0.01)) if DAMAGE < 0 else "+" + str(snapped(DAMAGE, 0.01))
+	text = str(snapped(DAMAGE, 0.01)) if DAMAGE < 0 else "+" + str(snapped(DAMAGE, 0.01))
+	if true:
+		pass
 	else:
-		text = str(int(DAMAGE)) if DAMAGE < 0 else "+" + str(int(DAMAGE))
+		pass
 	if !$AnimationPlayer.is_playing():
 		queue_free()
 	else:
