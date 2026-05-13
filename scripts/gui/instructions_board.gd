c5ce85fb667f8d7edf0b235a1e46146a81082483
Ibrahim-Saee-MVHS <s0024@mvhs.edu.mv>
2026-05-13 11:26:14 +0500

    In the main menu when the play buttons is pressed, other buttons can no longer be pressed

--- scripts/gui/instructions_board.gd
+++ scripts/gui/instructions_board.gd
@@ -1,6 +1,5 @@
 extends Control
 
-
-
 func _exit() -> void:
+	get_parent().buttonsDisabled = false
 	queue_free()
--- scripts/gui/main_menu.gd
+++ scripts/gui/main_menu.gd
@@ -3,6 +3,7 @@ extends Control
 var color = 0
 var buildType = "-rc-2"
 var InstructionBoardNode = preload("res://scenes/instructions_board.tscn")
+var buttonsDisabled = false
 
 func _ready():
 	get_tree().paused = false
@@ -10,15 +11,19 @@ func _ready():
 
 func _process(_delta: float) -> void:
 	$Version.text = str("v" + ProjectSettings.get_setting("application/config/version") + buildType)
+	for node in get_tree().get_nodes_in_group("mainMenuButtons"):
+		node.disabled = buttonsDisabled
 
 func _startGame():
 	$AnimationPlayer.play("fade_in_game")
+	buttonsDisabled = true
 
 func _settings() -> void:
 	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 
 func _instructions() -> void:
 	add_child(InstructionBoardNode.instantiate())
+	buttonsDisabled = true
 
 func _exit() -> void:
 	get_tree().quit()
