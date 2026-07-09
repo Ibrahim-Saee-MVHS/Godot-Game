f07463b7958152a225bdd36df9ba62f180adceff
Ibrahim-Saee-MVHS@Home <98446933+Finn10Plays@users.noreply.github.com>
2026-07-09 19:40:51 +0500

    Special Impossible description for Elemental Achievement

--- scripts/gui/achievement_card.gd
+++ scripts/gui/achievement_card.gd
@@ -15,7 +15,10 @@ func setAchievementCard():
 	$Card/Frame/Icon.texture = load("res://assets/sprites/achievement_icons/" + str(ID) + ".png")
 	$Card/Name.text = str(AchievementList.get(ID).name)
 	if AchievementList.get(ID).has("impossible") and AchievementList.get(ID).get("impossible") == true:
-		$Card/Description.parse_bbcode(str(AchievementList.get(ID).description) + "\n[color=#FF001A]This achievement is currently impossible, probably due to lack of content.[/color]")
+		if ID != "elemental":
+			$Card/Description.parse_bbcode(str(AchievementList.get(ID).description) + "\n[color=#FF001A]This achievement is currently impossible, probably due to lack of content.[/color]")
+		else:
+			$Card/Description.parse_bbcode(str(AchievementList.get(ID).description) + "\n[color=#FF4400]This achievement is possible, however I was unable to finish it by the time of this presentation.[/color]")
 	elif AchievementList.get(ID).has("warn") and AchievementList.get(ID).get("warn") == true:
 		$Card/Description.parse_bbcode(str(AchievementList.get(ID).description) + "\n[color=#FFEE88]This achievment hasn't been tested or obtained yet, there is a chance it is impossible.[/color]")
 	else:
