bd7b9354d6c03b62def3c4128a757b16511f2089
Ibrahim-Saee-MVHS <s0024@mvhs.edu.mv>
2026-07-13 10:48:54 +0500

    Trinity of Doom and Elementals Game Modifiers are now incompatible with each other (they already were but now you can't select both at the same time)

--- scripts/gui/game_modifiers.gd
+++ scripts/gui/game_modifiers.gd
@@ -15,10 +15,11 @@ func _ready():
 		var button = GameModifierButtonNode.instantiate()
 		button.ID = "tbd"
 		$ScrollContainer/CenterContainer/GridContainer.add_child(button)
-	lockModifiers()
+	
 
 func _process(_delta):
 	setDescriptionText()
+	lockModifiers()
 
 func _modifierSelected(button: BaseButton):
 	if Global.GAMEMODIFIERS.has(button.get_parent().ID):
@@ -28,6 +29,7 @@ func lockModifiers():
 	for button in $ScrollContainer/CenterContainer/GridContainer.get_children(false):
 		if button.ID == "tbd":
 			button.button_disabled = true
+		# achievement required game modifiers
 		if Global.gameModifierInfo.get(button.ID).has("achievements_required"):
 			var requirement = 0
 			for achievement in Global.gameModifierInfo.get(button.ID).get("achievements_required"):
@@ -41,6 +43,18 @@ func lockModifiers():
 				button.button_disabled = false
 			else:
 				button.button_disabled = true
+		# incompatible game modifiers
+		if Global.gameModifierInfo.get(button.ID).has("incompatiblilty_id"):
+			var id = Global.gameModifierInfo.get(button.ID).get("incompatiblilty_id")
+			for button2 in $ScrollContainer/CenterContainer/GridContainer.get_children(false):
+				if button2.ID == button.ID:
+					pass
+				elif Global.gameModifierInfo.get(button2.ID).get("incompatiblilty_id") == id:
+					if Global.GAMEMODIFIERS.get(button2.ID) == true:
+						button.button_disabled = true
+					else:
+						button.button_disabled = false
+						
 
 func setDescriptionText():
 	for button in $ScrollContainer/CenterContainer/GridContainer.get_children(false):
